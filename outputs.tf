9355a47ef26d1bfbfd4491cab52bb6954e2563bf
Jason Berlinsky <jason@barefootcoders.com>
2018-02-15 17:09:57 -0500

    Expose ALB information

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,11 @@
+output "vault_alb_addr" {
+  value = "${aws_alb.alb.dns_name}"
+}
+
+output "vault_alb_zone_id" {
+  value = "${aws_alb.alb.zone_id}"
+}
+
 output "vault_addr" {
   value = "${var.vault_dns_address}"
 }
